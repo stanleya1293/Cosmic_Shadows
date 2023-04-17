50b6b10a341b56db14fcddb9cf3e49ff89768f00
Ardeniscool1293 <arden.stanley@yahoo.com>
2023-04-17 16:34:48 -0500

    testing basic structure

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 CC = g++
 
 all: 
-	$(CC) -I include -L lib game.cpp -o cosmic_shadows -lmingw32 -lSDL2main -lSDL2
+	$(CC) -I include -L lib game.cpp -o cosmic_shadows -lmingw32 -lSDL2main -lSDL2 -lSDL2_image
--- game.cpp
+++ game.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 const int screenH = 700;
 const int screenW = 1000;
@@ -8,18 +9,28 @@ int main(int argc, char* argv[]) {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         std::cout << "Something went wrong when initializing";
     }
+    IMG_Init(IMG_INIT_PNG);
     SDL_Window* gameWin = SDL_CreateWindow("Cosmic Shadows", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenW, screenH, SDL_WINDOW_RESIZABLE);
     SDL_Renderer* gameRen = SDL_CreateRenderer(gameWin, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Surface* surface = IMG_Load("pixil-frame-0.png");
+    SDL_Texture* smiley = SDL_CreateTextureFromSurface(gameRen, surface);
+    SDL_RenderCopy(gameRen, smiley, NULL, NULL);
     SDL_Event* event;
-    bool gameRunning = 1;
-    while (gameRunning == 1) {
+    bool gameRunning = true;
+    while (gameRunning == true) {
         SDL_PollEvent(event);
+        SDL_RenderPresent(gameRen);
         if (event->type == SDL_WINDOWEVENT) {
-            if (event->window.event == SDL_WINDOWEVENT_LEAVE) {
-                gameRunning = 0;
+            if (event->window.event == SDL_WINDOWEVENT_CLOSE) {
+                gameRunning = false;
             }
         }
-        
     }
+    SDL_DestroyTexture(smiley);
+    SDL_DestroyWindow(gameWin);
+    SDL_DestroyRenderer(gameRen);
+    SDL_Quit();
+    IMG_Quit();
+
     return 0;
 }
